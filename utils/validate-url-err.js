fe8b1a186905c19a990ccbbfaf02bc456c9da2f4
Юленька <chervyak_9@mail.ru>
2023-06-23 21:44:42 +0300

    bugfix: 14 спринт, первое ревью, регулярка

--- utils/validate-url-err.js
+++ utils/validate-url-err.js
@@ -1,7 +1,10 @@
 /* eslint-disable no-useless-escape */
 /* eslint-disable arrow-body-style */
 /* eslint-disable linebreak-style */
-const reg = /https?:\/\/w?w?w?[a-z.\/0-9\-\_\~\:\/\?\[\]\@\!\$\&\'\(\)\*\+\,\;\=]+#?/i;
+
+// const reg = /https?:\/\/w?w?w?[a-z.\/0-9\-\_\~\:\/\?\[\]\@\!\$\&\'\(\)\*\+\,\;\=]+#?/i;
+
+const reg = /https?:\/\/(www\.)?([A-z0-9-]{2,63}\.)+[A-z0-9]{1,6}[A-z.\/0-9\-\_\~\:\/\?\[\]\@\!\$\&\'\(\)\*\+\,\;\=]+#?/i;
 const validateURL = (v) => {
   return reg.test(v);
 };
